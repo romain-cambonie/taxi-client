2555e66eb8dbb00b0723675edbf307803d140fe0
Marc Gavanier <marc.gavanier@gmail.com>
2023-02-20 03:25:16 +0100

    feat: add main layout

--- src/root/layouts/application-root/application-root.layout.ts
+++ src/root/layouts/application-root/application-root.layout.ts
@@ -3,6 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
-  templateUrl: './application-root-layout.component.html'
+  templateUrl: './application-root.layout.html'
 })
 export class ApplicationRootLayout {}
