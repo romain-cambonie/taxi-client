c392d7b76a58ef3bfaca29322816bc3d7f7b1927
Marc Gavanier <marc.gavanier@gmail.com>
2023-02-20 03:25:05 +0100

    refactor: rename static to public feature

--- src/features/static/modules/static.feature.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { PAGES } from '../pages';
-import { StaticFeatureRoutingModule } from './static.feature-routing.module';
-
-@NgModule({
-  declarations: [...PAGES],
-  imports: [StaticFeatureRoutingModule]
-})
-export class StaticFeatureModule {}
