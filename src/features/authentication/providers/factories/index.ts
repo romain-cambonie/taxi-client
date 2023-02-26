0aaef1340f1c269a7bc860e48c5acfd50233330e
Marc Gavanier <marc.gavanier@gmail.com>
2023-02-26 14:53:37 +0100

    feat: add activate factory provider

--- src/features/authentication/providers/factories/index.ts
+++ src/features/authentication/providers/factories/index.ts
@@ -1,3 +1,4 @@
+export * from './activate.factory.provider';
 export * from './forgot-password.factory.provider';
 export * from './login.factory.provider';
 export * from './register.factory.provider';
