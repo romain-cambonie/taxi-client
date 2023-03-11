a69bb03ee365c2541ee0cd17f0e157a45f89162e
Marc Gavanier <marc.gavanier@gmail.com>
2023-03-11 16:03:46 +0100

    feat: replace in memory fares with api call

--- src/root/providers/planning.providers.ts
+++ src/root/providers/planning.providers.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { faresByDayRead$, faresByDayReadProvider } from '@features/planning';
 
-export const PLANNING_PROVIDERS = [faresByDayReadProvider(faresByDayRead$)];
+export const PLANNING_PROVIDERS = [faresByDayReadProvider(faresByDayRead$, [HttpClient])];
