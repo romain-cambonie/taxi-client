40799ece2558ed01b7c74d0d72c645bf4688658c
Marc Gavanier <marc.gavanier@gmail.com>
2023-03-18 15:15:43 +0100

    feat: remove in memory forgot password action from providers and register reset password action

--- src/root/providers/authentication.providers.ts
+++ src/root/providers/authentication.providers.ts
@@ -1,28 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import {
-  ACCOUNTS_PERSISTENCE,
-  accountsValueProvider,
   activateActionProvider,
   forgotPasswordActionProvider,
-  inMemoryForgotPasswordAction$,
   loginActionProvider,
   registerActionProvider,
   resendActivationCodeActionProvider,
+  resetPasswordActionProvider,
   SESSION_PERSISTENCE
 } from '@features/authentication';
 import {
   COGNITO_PERSISTENCE,
   cognitoActivateAction$,
+  cognitoForgotPasswordAction$,
   cognitoLoginAction$,
   cognitoRegisterAction$,
-  cognitoResendActivationCodeAction$
+  cognitoResendActivationCodeAction$,
+  cognitoResetPasswordAction$
 } from '@features/aws';
 
 export const AUTHENTICATION_PROVIDERS = [
-  accountsValueProvider(),
-  forgotPasswordActionProvider(inMemoryForgotPasswordAction$, [ACCOUNTS_PERSISTENCE]),
+  forgotPasswordActionProvider(cognitoForgotPasswordAction$, [HttpClient, COGNITO_PERSISTENCE]),
   activateActionProvider(cognitoActivateAction$, [HttpClient, COGNITO_PERSISTENCE]),
   loginActionProvider(cognitoLoginAction$, [HttpClient, COGNITO_PERSISTENCE, SESSION_PERSISTENCE]),
   registerActionProvider(cognitoRegisterAction$, [HttpClient, COGNITO_PERSISTENCE]),
-  resendActivationCodeActionProvider(cognitoResendActivationCodeAction$, [HttpClient, COGNITO_PERSISTENCE])
+  resendActivationCodeActionProvider(cognitoResendActivationCodeAction$, [HttpClient, COGNITO_PERSISTENCE]),
+  resetPasswordActionProvider(cognitoResetPasswordAction$, [HttpClient, COGNITO_PERSISTENCE])
 ];
