6e0e7bc3fc2d482bc6696528e82a97066aa0cd02
Romain Cambonie <romain.cambonie@gmail.com>
2023-03-29 09:24:30 +0200

    feat: read fare transfer matches the payload structure

--- src/features/planning/reads/fares-by-day/fares-by-day.read.ts
+++ src/features/planning/reads/fares-by-day/fares-by-day.read.ts
@@ -2,36 +2,40 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { FareByDayPresentation, FareStatus } from '../../presentation';
 
-type FareTransfer = {
-  rid: string;
-  created_at: string;
-  creator: string;
+export type FareTransfer = {
+  clientComment: string | undefined;
+  clientIdentity: string;
+  clientPhone: string;
+  createdAt: string;
+  creatorIdentity: string;
   date: string;
-  distance: string;
+  driveDistanceInMeters: string;
+  driveComment: string | undefined;
+  driveDistanceOverride: string | undefined;
+  driveFrom: string;
+  driveKind: 'one-way' | 'outward' | 'return';
+  driveName: string;
+  driveNature: 'medical' | 'standard';
+  driveRid: string;
+  driverIdentity: string | undefined;
+  driveTo: string;
   duration: string;
-  in_has_entry: string;
-  in_has_fare: string;
-  isreturn: string;
-  locked: string;
-  meters: string;
-  out_has_invoice: string;
-  recurrent: string;
+  rid: string;
+  startTime: string;
   status: string;
-  subcontractor: string;
-  time: string;
-  timestamp: string;
-  updated_at: string;
-  weeklyrecurrence: string;
+  subcontractorIdentity: string | undefined;
+  updatedAt: string;
+  weeklyRecurrence: string;
 };
 
 const toFareByDayPresentation = (fares: FareTransfer[]): FareByDayPresentation[] =>
   fares.map((fare: FareTransfer) => ({
     id: fare.rid,
     date: fare.date,
-    distance: fare.distance,
+    distance: fare.driveDistanceInMeters,
     duration: fare.duration,
     status: fare.status as FareStatus,
-    startTime: fare.time
+    startTime: fare.startTime
   }));
 
 export const faresByDayRead$ = (httpClient: HttpClient) => (): Observable<FareByDayPresentation[]> =>
